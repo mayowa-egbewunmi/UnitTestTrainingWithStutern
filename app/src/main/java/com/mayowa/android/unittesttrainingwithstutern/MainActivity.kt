2f801f804d62d4753c4d0560a5b80ca758889789
Mayowa Egbewunmi <mayowaegbewunmi@gmail.com>
2020-03-23 13:31:52 +0100

    Project set up

---
+++ app/src/main/java/com/mayowa/android/unittesttrainingwithstutern/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mayowa.android.unittesttrainingwithstutern
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
